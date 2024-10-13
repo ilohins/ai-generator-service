79e2d83c55e1f36270103dccc7cf8235ec50ed99
Igors Lohins <ilohins@users.noreply.github.com>
2024-10-13 15:57:44 -0400

    Project skeleton generated by Spring Initlzr

---
+++ src/main/java/net/l3ns/ai_magnet_generator_service/AiMagnetGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package net.l3ns.ai_magnet_generator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMagnetGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMagnetGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/l3ns/ai_magnet_generator_service/AiMagnetGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.l3ns.ai_magnet_generator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMagnetGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
